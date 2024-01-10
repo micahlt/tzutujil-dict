70d23da9cf3d68aabeaebd873ccd6c20eb517fda
Micah Lindley <hi@micahlindley.com>
2024-01-10 00:55:29 +0000

    Add word list

--- src/app/page.js
+++ src/app/page.js
@@ -4,11 +4,15 @@ import styles from "./home.module.css";
 
 export default function Home() {
   const [count, setCount] = useState(1);
+  const [words, setWords] = useState([]);
   useEffect(() => {
     fetch("/api/getCount")
       .then((res) => res.text())
       .then((text) => setCount(text));
-  });
+    fetch("/api/getAll")
+      .then((res) => res.json())
+      .then((json) => setWords(json));
+  }, []);
   return (
     <main>
       <div className={styles.hero}>
@@ -18,6 +22,13 @@ export default function Home() {
             Over <span>{count - 1}</span> translated words
           </h2>
         </div>
+      </div>
+      <div className={styles.allWords}>
+        <ul>
+          {words.map((word) => (
+            <li key={word.id}>{word.tzWord}</li>
+          ))}
+        </ul>
       </div>
     </main>
   );
