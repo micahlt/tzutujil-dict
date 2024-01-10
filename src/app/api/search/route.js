cc968715429de189e31b5bfb3f0295d372a85ed9
Micah Lindley <hi@micahlindley.com>
2024-01-10 16:11:42 +0000

    Basic search functionality

--- src/app/api/search/route.js
+++ src/app/api/search/route.js
@@ -7,11 +7,17 @@ export async function GET(req) {
   // Create the connection to the database
   const connection = mysql.createConnection(process.env.PLANET_URL);
   const searchParams = req.nextUrl.searchParams;
-  const limit = searchParams.get("limit");
+  const query = `%${searchParams.get("q")}%`.replace("'", "’");
+  const autoComplete = searchParams.get("ac");
 
   // simple query
   const results = await connection
     .promise()
-    .query(`SELECT * FROM words${limit ? " LIMIT " + limit : ""}`);
+    .execute(
+      `SELECT * FROM words WHERE tzWord LIKE ? OR esWord LIKE ? OR enWord LIKE ? ORDER BY tzWord ${
+        autoComplete ? "LIMIT 5" : ""
+      };`,
+      [query, query, query]
+    );
   return Response.json(results[0]);
 }
--- src/app/page.js
+++ src/app/page.js
@@ -7,14 +7,23 @@ import Navbar from "@/components/Navbar";
 export default function Home() {
   const [count, setCount] = useState(1);
   const [words, setWords] = useState([]);
+  const [searchFocused, setSearchFocused] = useState(false);
+  const [results, setResults] = useState([]);
   useEffect(() => {
     fetch("/api/getCount")
       .then((res) => res.text())
       .then((text) => setCount(text));
-    fetch("/api/getAll")
+    fetch("/api/getAll?limit=30")
       .then((res) => res.json())
       .then((json) => setWords(json));
   }, []);
+  const searchChange = (e) => {
+    fetch(`/api/search?q=${e.target.value}&ac=true`)
+      .then((res) => res.json())
+      .then((json) => {
+        setResults(json);
+      });
+  };
   return (
     <>
       <Navbar />
@@ -26,6 +35,38 @@ export default function Home() {
               {local.t("moreThan")} <span>{count - 1}</span>{" "}
               {local.t("translatedWords")}
             </h2>
+            <div className={styles.heroSearchWrapper}>
+              <input
+                type="search"
+                className={styles.heroSearch}
+                placeholder="Search across languages..."
+                onBlur={() => setSearchFocused(false)}
+                onFocus={() => setSearchFocused(true)}
+                onChange={searchChange}
+              ></input>
+              {searchFocused && (
+                <div className={styles.searchSuggestions}>
+                  {results.map((res) => (
+                    <div key={res.id} className={styles.suggestion}>
+                      <h3>{res.tzWord}</h3>
+                      <p>
+                        {res.esWord && (
+                          <>
+                            <b>ES</b> {res.esWord}
+                          </>
+                        )}
+                        {res.esWord && res.enWord && " | "}
+                        {res.enWord && (
+                          <>
+                            <b>EN</b> {res.enWord}
+                          </>
+                        )}
+                      </p>
+                    </div>
+                  ))}
+                </div>
+              )}
+            </div>
           </div>
         </div>
         <div className={styles.allWords}>
