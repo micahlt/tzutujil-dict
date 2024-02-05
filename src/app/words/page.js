b1bcf81d8bc5ca637506702b6cf4c8d5f9033893
Micah Lindley <hi@micahlindley.com>
2024-02-05 15:40:20 +0000

    Add query params to word page

--- src/app/words/page.js
+++ src/app/words/page.js
@@ -5,20 +5,32 @@ import Navbar from "@/components/Navbar";
 import Link from "next/link";
 import local from "@/app/i18n";
 import { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 
 export default function Words() {
   const nav = useRouter();
-  const [page, setPage] = useState(1);
-  const [perPage, setPerPage] = useState(25);
+  const params = useSearchParams();
+  const [page, setPage] = useState();
+  const [perPage, setPerPage] = useState();
   const [listData, setListData] = useState(null);
+  const [hasFetchedParams, setHasFetchedParams] = useState(false);
   useEffect(() => {
-    fetch(`/api/getAll?limit=${perPage}&offset=${(page - 1) * 25}`)
-      .then((res) => res.json())
-      .then((json) => {
-        setListData(json);
-      });
+    if (hasFetchedParams && page && perPage) {
+      nav.push(`/words?perPage=${perPage}&page=${page}`);
+      fetch(`/api/getAll?limit=${perPage}&offset=${(page - 1) * perPage}`)
+        .then((res) => res.json())
+        .then((json) => {
+          setListData(json);
+        });
+    }
   }, [page, perPage]);
+  useEffect(() => {
+    if (params && !hasFetchedParams) {
+      setPage(Number(params.get("page") || 1));
+      setPerPage(Number(params.get("perPage") || 50));
+      setHasFetchedParams(true);
+    }
+  }, [params]);
   return (
     <>
       <Navbar />
@@ -40,8 +52,10 @@ export default function Words() {
               </thead>
               <tbody>
                 {listData.map((word) => (
-                  <tr onClick={() => nav.push(`/words/${word.id}`)}>
-                    <td>{word.tzWord}</td>
+                  <tr key={word.id}>
+                    <td>
+                      <Link href={`/words/${word.id}`}>{word.tzWord}</Link>
+                    </td>
                     <td>{word.esWord}</td>
                     <td>{word.enWord}</td>
                     <td>{word.id}</td>
@@ -51,38 +65,42 @@ export default function Words() {
             </table>
           )}
           <div className={styles.divider}></div>
-          <footer className={styles.footer}>
-            <div className={styles.pageNav}>
-              Page{" "}
-              <input
-                type="text"
-                value={page}
-                onChange={(e) => setPage(e.target.value)}
-              />
-              {"  "}showing{"  "}
-              <input
-                type="text"
-                value={perPage}
-                onChange={(e) => setPerPage(e.target.value)}
-              />{" "}
-              per page
-            </div>
-            <div className={styles.navButtons}>
-              <ArrowLeftCircle
-                size={24}
-                onClick={() => {
-                  if (page > 1) setPage(page - 1);
-                }}
-                opacity={page > 1 ? 1 : 0.3}
-                cursor={page > 1 ? "default" : "pointer"}
-              ></ArrowLeftCircle>
-              <ArrowRightCircle
-                size={24}
-                onClick={() => setPage(page + 1)}
-                cursor="pointer"
-              ></ArrowRightCircle>
-            </div>
-          </footer>
+          {page && perPage && (
+            <footer className={styles.footer}>
+              <div className={styles.pageNav}>
+                Page{" "}
+                <input
+                  type="text"
+                  value={page}
+                  onChange={(e) => setPage(e.target.value)}
+                  suppressHydrationWarning={true}
+                />
+                {"  "}showing{"  "}
+                <input
+                  type="text"
+                  value={perPage}
+                  onChange={(e) => setPerPage(e.target.value)}
+                  suppressHydrationWarning={true}
+                />{" "}
+                per page
+              </div>
+              <div className={styles.navButtons}>
+                <ArrowLeftCircle
+                  size={24}
+                  onClick={() => {
+                    if (page > 1) setPage(page - 1);
+                  }}
+                  opacity={page > 1 ? 1 : 0.3}
+                  cursor={page > 1 ? "default" : "pointer"}
+                ></ArrowLeftCircle>
+                <ArrowRightCircle
+                  size={24}
+                  onClick={() => setPage(page + 1)}
+                  cursor="pointer"
+                ></ArrowRightCircle>
+              </div>
+            </footer>
+          )}
         </div>
       </main>
     </>
