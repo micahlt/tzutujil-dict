7c1c86f8145f89778efe343b6ae03808af857d51
Micah Lindley <hi@micahlindley.com>
2024-02-05 20:19:59 +0000

    Add Twitter metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,6 +13,16 @@ export const metadata = {
   openGraph: {
     image: [`https://tzdb.micahlindley.com/api/og`],
   },
+  twitter: {
+    card: "summary_large_image",
+    title: "TzDB | Tz'utujil Language Database",
+    description:
+      "The world's largest, most comprehensive Tz'utujil dictionary and translator.",
+    siteId: "1561359226819198976",
+    creator: "@micahtlindley",
+    creatorId: "1561359226819198976",
+    images: ["https://tzdb.micahlindley.com/api/og"], // Must be an absolute URL
+  },
 };
 
 export default function RootLayout({ children }) {
