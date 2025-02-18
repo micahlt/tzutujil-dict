05c6a879185655078c2be41097c1fd15be7c1340
Micah Lindley <hi@micahlindley.com>
2025-02-18 14:07:01 -0600

    Better search results using autocomplete suggester

--- src/app/api/search/route.js
+++ src/app/api/search/route.js
@@ -24,5 +24,63 @@ export async function GET(req) {
       field: "id",
     },
   });
-  return Response.json(results.hits.hits.flatMap((v) => v._source));
+  const suggestions = await client.search({
+    index: process.env.ELASTIC_WORD_INDEX,
+    q: query,
+    suggest: {
+      suggest0: {
+        prefix: query,
+        completion: {
+          field: "variants",
+          size: 10,
+          skip_duplicates: true,
+        },
+      },
+      suggest1: {
+        prefix: query,
+        completion: {
+          field: "definitions.en.translation",
+          size: 10,
+          skip_duplicates: true,
+        },
+      },
+      suggest2: {
+        prefix: query,
+        completion: {
+          field: "definitions.es.translation",
+          size: 10,
+          skip_duplicates: true,
+        },
+      },
+    },
+  });
+  let suggs = [];
+  Object.keys(suggestions.suggest).forEach((sugKey) => {
+    const opts = suggestions.suggest[sugKey][0].options;
+    if (opts.length == 0) {
+      return;
+    } else {
+      opts.forEach((opt) => {
+        suggs.push(opt._source);
+      });
+    }
+  });
+  const suggsResults = suggs.filter(
+    (obj1, i, arr) => arr.findIndex((obj2) => obj2.id === obj1.id) === i
+  );
+  const searchResults = results.hits.hits.flatMap((v) => v._source);
+  let finalArr = [];
+  console.log(suggsResults);
+  searchResults.forEach((result) => {
+    if (!finalArr.some((r) => r.id === result.id)) {
+      finalArr.push(result);
+    }
+  });
+  suggsResults.forEach((result) => {
+    if (!finalArr.some((r) => r.id === result.id)) {
+      finalArr.push(result);
+    }
+  });
+
+  return Response.json(finalArr);
 }
